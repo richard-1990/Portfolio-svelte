a530216325e5637e67974fe7ba4b1f9a95bf4829
Richard Price <richard.price@littlestarmedia.com>
2023-12-18 13:41:21 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">RP</h2>
+		<div class="flex justify-center space-x-2">
+			<a class="btn variant-filled" href="https://skeleton.dev/" target="_blank" rel="noreferrer">
+				Launch Documentation
+			</a>
+		</div>
+	</div>
+</div>
